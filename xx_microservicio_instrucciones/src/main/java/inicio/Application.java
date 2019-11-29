4a9f46031d4a43cf677a6a7610806d04129c222f
tarde <tarde@G24-EQ10>
2019-11-29 17:45:17 +0100

    version2

--- xx_microservicio_instrucciones/src/main/java/inicio/Application.java
+++ xx_microservicio_instrucciones/src/main/java/inicio/Application.java
@@ -18,25 +18,6 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();				
-	}
 	
-	public RestTemplate getRestTemplateSSL() {
-		//return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
-		RestTemplate rest = new RestTemplate();		
-		/*
-		System.setProperty("javax.net.ssl.keyStore", "C:\\Program Files\\Java\\jdk1.8.0_11\\bin\\keystore.jks");
-		System.setProperty("javax.net.ssl.keyStorePassword", "justicia");
-		*/
-		/*
-        System.setProperty("javax.net.ssl.trustStore", "C:\\Program Files\\Java\\jdk1.8.0_181\\jre\\lib\\security\\cacerts");
-        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
-        System.setProperty("logging.level.org.springframework.security","DEBUG");
-        */
-        		
-        return rest;
-	}
 
 }
